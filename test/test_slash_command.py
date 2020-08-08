d90c841262648569be1b36d8a435d7bf91bfb01a
yoshiki <yoshiki0911@gmail.com>
2020-08-08 14:34:10 +0900

    testの修正

--- test/test_slash_command.py
+++ test/test_slash_command.py
@@ -23,42 +23,59 @@ def generate_slash_command_payload(
         'trigger_id': ['[0-9]{13}.[0-9]{12}.[0-9a-z]+']}
 
 
+# instance
 sc1 = SlashCommand("test1")
 
-
-@sc1.add(command="/sc1", condition=lambda x: x['user_id'][0] == "B", after=lambda x: f"after_{x}_user_B")
-@sc1.add(command="/sc1")
-def sc1_accept(params):
-    return "sc1_accept"
-
-
-@sc1.add(command="/sc1", condition=lambda x: x['user_id'][0] == "A")
-def sc1_accept_user_a(params):
-    return "sc1_accept_user_A"
+# command list
+cmd1 = "/command1"
+cmd2 = "/command2"
+cmd3 = "/command3"
 
 
-@sc1.add(command="/sc1", condition=lambda x: x['channel_id'][0] == "Z")
-def sc1_accept_user_a(params):
-    return "sc1_accept_channel_Z"
+# accept lambda
+def accept_user_x(user) -> callable:
+    return lambda x: f"accept_{x}_user_{user}"
 
 
-@sc1.add(command="/sc2")
+# accept lambda
+def accept_channel_x(channel) -> callable:
+    return lambda x: f"accept_{x}_user_{channel}"
+
+
+@sc1.add(command=cmd1, condition=lambda x: x['user_id'][0] == "A", after=accept_user_x("A"))
+@sc1.add(command=cmd1, user_id="B", after=accept_user_x("B"))
+@sc1.add(command=cmd1, user_id=["C", "D"], after=accept_user_x("CD"))
+@sc1.add(command=cmd1, condition=lambda x: x['channel_id'][0] == "Z", after=accept_channel_x("Z"))
+@sc1.add(command=cmd1, channel_id="Y", after=accept_channel_x("Y"))
+@sc1.add(command=cmd1, channel_id=["W", "X"], after=accept_channel_x("WX"))
+@sc1.add(command=cmd1)
+def accept_cmd1(params):
+    return cmd1
+
+
+@sc1.add(command=cmd2)
 def sc2_accept(params):
-    return "sc2_accept"
+    return cmd2
 
 
-@sc1.add(command="/sc3", guard=True)
+@sc1.add(command=cmd3, guard=True)
 def sc2_accept(params):
-    return "guard"
+    return cmd3
 
 
 @pytest.mark.parametrize("slack_payload,ideal_result", [
-        (generate_slash_command_payload(command="/sc1"), "sc1_accept"),
-        (generate_slash_command_payload(command="/sc2"), "sc2_accept"),
-        (generate_slash_command_payload(command="/sc1", user_id="B"), "after_sc1_accept_user_B"),
-        (generate_slash_command_payload(command="/sc1", user_id="A"), "sc1_accept_user_A"),
-        (generate_slash_command_payload(command="/sc1", channel_id="Z"), "sc1_accept_channel_Z"),
-        (generate_slash_command_payload(command="/none"), "guard")
+        (generate_slash_command_payload(command=cmd1), cmd1),
+        (generate_slash_command_payload(command=cmd2), cmd2),
+        (generate_slash_command_payload(command=cmd1, user_id="A"), accept_user_x("A")(cmd1)),
+        (generate_slash_command_payload(command=cmd1, user_id="B"), accept_user_x("B")(cmd1)),
+        (generate_slash_command_payload(command=cmd1, user_id="C"), accept_user_x("CD")(cmd1)),
+        (generate_slash_command_payload(command=cmd1, user_id="D"), accept_user_x("CD")(cmd1)),
+        (generate_slash_command_payload(command=cmd1, channel_id="Z"), accept_channel_x("Z")(cmd1)),
+        (generate_slash_command_payload(command=cmd1, channel_id="Y"), accept_channel_x("Y")(cmd1)),
+        (generate_slash_command_payload(command=cmd1, channel_id="W"), accept_channel_x("WX")(cmd1)),
+        (generate_slash_command_payload(command=cmd1, channel_id="X"), accept_channel_x("WX")(cmd1)),
+        (generate_slash_command_payload(command=cmd3), cmd3),
+        (generate_slash_command_payload(command="/none"), cmd3)
     ])
 def test_slash_command(slack_payload, ideal_result):
     assert sc1.execute(slack_payload) == ideal_result
