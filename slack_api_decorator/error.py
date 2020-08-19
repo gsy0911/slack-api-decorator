d95efb125557c8f18f0023ab4edc80353d4a5954
yoshiki <yoshiki0911@gmail.com>
2020-08-19 23:44:00 +0900

    #6 slackのparameterに関するErrorを追加

--- slack_api_decorator/error.py
+++ slack_api_decorator/error.py
@@ -1,2 +1,11 @@
 class SlackApiDecoratorException(Exception):
     pass
+
+
+class SlackParameterNotFoundError(SlackApiDecoratorException):
+    def __init__(self, missing_key: str, parent_dict: dict):
+        self.missing_key = missing_key
+        self.parent_dict = parent_dict
+
+    def __str__(self):
+        return f"[{self.missing_key}] not found in [{self.parent_dict}]"
