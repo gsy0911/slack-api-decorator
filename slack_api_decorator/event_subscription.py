bde91569b960f0fb68593e51759bfb8a6297419e
gsy0911 <gsy0911@users.noreply.github.com>
2020-08-30 17:12:15 +0900

    change class_method to instance_method

--- slack_api_decorator/event_subscription.py
+++ slack_api_decorator/event_subscription.py
@@ -32,11 +32,11 @@ class EventSubscription:
         ...         }
         >>> es.execute(params=payload_from_slack)
     """
-    ignore_user_id_list = []
 
     def __init__(self, app_name: str):
         self.app_name = app_name
         self._executor_list = []
+        self.ignore_user_id_list = []
         
     @staticmethod
     def _get_event(params: dict) -> dict:
@@ -174,9 +174,14 @@ class EventSubscription:
 
         return decorator
 
-    @classmethod
-    def add_ignore_user_id_list(cls, user_id: str):
-        cls.ignore_user_id_list.append(user_id)
+    def add_ignore_user_id_list(self, user_id: str):
+        """
+        add ignore user list to avoid being invoked by the app-user
+        
+        Args:
+            user_id: app-user user_id is recommended.
+        """
+        self.ignore_user_id_list.append(user_id)
 
     def execute(self, params: dict):
         """
